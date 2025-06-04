6387f92796310c559696efca18a9e3d112b41aa4
Renako223678 <134682228+Renako223678@users.noreply.github.com>
2025-06-04 11:21:28 +0700

    add user api

---
+++ Service/Implement/UserService.cs
@@ -0,0 +1,52 @@
+﻿using Repository.Models;
+using Repository.Repository;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Implement
+{
+    public class UserService : IUserService
+    {
+        private readonly UserRepository _repository;
+        public UserService(UserRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<int> CreateAsync(User user)
+        {
+            return await _repository.CreateAsync(user);
+        }
+
+        public async Task<bool> DeleteByIdAsync(int UserId)
+        {
+            var user = _repository.GetById(UserId);
+            if (user == null)
+            {
+                return false;
+            }
+            await _repository.RemoveAsync(user);
+             await _repository.SaveAsync();
+            return true;
+        }
+
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int UserId)
+        {
+            return await _repository.GetByIdAsync(UserId);
+        }
+
+        public async Task<int> UpdateAsync(User user)
+        {
+            await _repository.UpdateAsync(user);
+            return await _repository.SaveAsync(); 
+        }
+    }
+}
---
+++ Service/Interface/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interface
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetAllAsync();
+        Task<User> GetByIdAsync(int UserId);
+        Task<int> UpdateAsync(User user);
+        Task<int> CreateAsync(User user);
+        Task<bool> DeleteByIdAsync(int UserId);
+    }
+}
