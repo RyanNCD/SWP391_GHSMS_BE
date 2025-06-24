0b38592613ecfdd7bbb28fce62a9e251edc4f72a
RyanNCD <danh.11x2.nguyen@gmail.com>
2025-06-24 21:02:51 +0700

    fix

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4864d1a1f854aac7357aa481ef2fe3aec064a754")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184956c4eb0bd776df71e020108e7d07cf573159")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
