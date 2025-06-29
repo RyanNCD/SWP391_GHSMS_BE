6f7a9f4790d9ed20f84983644115a2a505766d95
dothephi <phidtse161229@fpt.edu.vn>
2025-06-30 04:10:08 +0700

    "Done Email Sender"

---
+++ EmailService/DTO/EmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace EmailService.DTO
+{
+    public class EmailRequest<T>
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public T Body { get; set; }
+    }
+}
